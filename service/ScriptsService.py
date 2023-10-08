93ebcb35cc48e745a27f708f5570502fd7238b73
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2023-10-08 13:27:32 -0300

    Change variable name for end date in ScriptsService.py
    Replaced the parameter 'data_end' with 'data_fim' in the ScriptsService.py module to match naming conventions used in the rest of the project. This adjustment will avoid potential KeyError exceptions when accessing this parameter and assists with accurate debugging.

--- service/ScriptsService.py
+++ service/ScriptsService.py
@@ -28,7 +28,7 @@ class ScriptsService(Interceptor):
         if 'data_ini' not in params_:
             params_['data_ini'] = '2020-01-01'
         if 'data_end' not in params_:
-            params_['data_end'] = datetime.now().strftime("%Y-%m-%d")
+            params_['data_fim'] = datetime.now().strftime("%Y-%m-%d")
 
         for key in params_:
             script = script.replace(":" + key, "'" + params_[key] + "'")
